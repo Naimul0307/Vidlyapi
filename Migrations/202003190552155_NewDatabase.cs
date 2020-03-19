a183645e10f8943bc62b8a4ce852c48d39c68386
Naim0307 <mdnaimul15902@gmail.com>
2020-03-19 21:52:54 +0600

    Add_Jquary_Date_Picker

---
+++ Migrations/202003190552155_NewDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace VidlyApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class NewDatabase : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -18,9 +18,13 @@ namespace VidlyApi.Models
         [Display(Name = "Genre")]
         [Required]
         public byte GenreId { get; set; }
+
+        [Display(Name = "Date Of Add")]
         public DateTime DateAdded { get; set; }
+
         [Display(Name="Release Date")]
         public DateTime ReleaseDate { get; set; }
+
         [Display (Name="Number In Stock")]
         public byte NumberInStock { get; set; }
         public byte NumberAvailable { get; set; }
